e70047fa8f378b7e10e85f0cf8fc01ee5a4c38be
EvgeniyPerm <112462287+EvgeniyPerm@users.noreply.github.com>
2022-09-13 21:00:30 +0500

    You are robbing a bank, but you’re not taking everything. You are looking for a specific item in the safety deposit boxes and you are going to drill into each one in order to find your item. Once you find your item you can make your escape, but how long will it take you to get to that item?
    Task
    Determine the amount of time it will take you to find the item you are looking for if it takes you 5 minutes to drill into each box.
    
    Input Format
    A string that represent the items in each box that will be drilled in order (items are separated by a comma), and secondly, a string of which item you are looking for.
    
    Output Format
    An integer of the amount of time it will take for you to find your item.
    
    Sample Input
    'gold,diamonds,documents,Declaration of Independence,keys'
    'Declaration of Independence'
    
    Sample Output
    20

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        String[] boxes = scanner.nextLine().split(",");
+        String item = scanner.nextLine();
+        int i=0;
+        for (String box:boxes) {
+            if (box.equals(item)) break;
+            i++;
+        };
+        System.out.println(5*i+5);
     }
 }
